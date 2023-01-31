2a97b009fdc49fb50eb27edbb023a6aa0d396816
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2023-02-01 00:25:49 +0100

    Check validity of start and target cell

--- aStarUnity/Assets/Scripts/Grid.cs
+++ aStarUnity/Assets/Scripts/Grid.cs
@@ -8,10 +8,10 @@ public class Grid : MonoBehaviour {
   public int cellsPerRow = 10;
   public int startCellID = 12;
   public int targetCellID = 87;
-  
+
   [HideInInspector] public bool isCalculating;
   public List<Cell> allCells;
-  
+
   private int numberOfCells;
   private List<Cell> openList;
   private List<Cell> closedList;
@@ -64,79 +64,81 @@ public class Grid : MonoBehaviour {
     CreateStart(startCellID);
     CreateTarget(targetCellID);
 
-    openList = new List<Cell>();
-    closedList = new List<Cell>();
+    if (startCell.isValid && targetCell.isValid) {
+      openList = new List<Cell>();
+      closedList = new List<Cell>();
 
-    Cell currentCell = startCell;
-    AddCellToClosedList(currentCell);
-
-    const float cycleDelay = 0f;
-    int cycleCounter = 0;
-    while (currentCell.id != targetCell.id) {
-      yield return new WaitForSeconds(cycleDelay);
-
-      // Safety-abort in case of endless loop
-      cycleCounter++;
-      if (cycleCounter >= numberOfCells) {
-        Debug.Log("No Path Found");
-        break;
-      }
-
-      // Add all cells adjacent to currentCell to openList
-      foreach (Cell cell in GetAdjacentCells(currentCell)) {
-        float tentativeG = currentCell.G + Vector3.Distance(currentCell.transform.position, cell.transform.position);
-        // If cell is on closed list skip to next cycle
-        if (cell.onClosedList && tentativeG > cell.G) {
-          continue;
-        }
-
-        if (!cell.onOpenList || tentativeG < cell.G) {
-          cell.CalculateH(targetCell);
-          cell.G = tentativeG;
-          cell.F = cell.G + cell.H;
-          cell.parent = currentCell;
-
-          if (!cell.onClosedList)
-            AddCellToOpenList(cell);
-        }
-      }
-
-      yield return new WaitForSeconds(cycleDelay);
-
-      // Get cell with lowest F value from openList, set it to currentCell
-      float lowestFValue = 99999.9f;
-      foreach (Cell cell in openList.Where(cell => cell.F < lowestFValue)) {
-        lowestFValue = cell.F;
-        currentCell = cell;
-      }
-
-      // Remove currentCell from openList, add to closedList
-      openList.Remove(currentCell);
+      Cell currentCell = startCell;
       AddCellToClosedList(currentCell);
-    }
 
-    // Get path
-    List<Cell> path = new ();
-    currentCell = targetCell;
-    
-    while (currentCell.id != startCell.id) {
+      const float cycleDelay = 0f;
+      int cycleCounter = 0;
+      while (currentCell.id != targetCell.id) {
+        yield return new WaitForSeconds(cycleDelay);
+
+        // Safety-abort in case of endless loop
+        cycleCounter++;
+        if (cycleCounter >= numberOfCells) {
+          Debug.Log("No Path Found");
+          break;
+        }
+
+        // Add all cells adjacent to currentCell to openList
+        foreach (Cell cell in GetAdjacentCells(currentCell)) {
+          float tentativeG = currentCell.G + Vector3.Distance(currentCell.transform.position, cell.transform.position);
+          // If cell is on closed list skip to next cycle
+          if (cell.onClosedList && tentativeG > cell.G) {
+            continue;
+          }
+
+          if (!cell.onOpenList || tentativeG < cell.G) {
+            cell.CalculateH(targetCell);
+            cell.G = tentativeG;
+            cell.F = cell.G + cell.H;
+            cell.parent = currentCell;
+
+            if (!cell.onClosedList)
+              AddCellToOpenList(cell);
+          }
+        }
+
+        yield return new WaitForSeconds(cycleDelay);
+
+        // Get cell with lowest F value from openList, set it to currentCell
+        float lowestFValue = 99999.9f;
+        foreach (Cell cell in openList.Where(cell => cell.F < lowestFValue)) {
+          lowestFValue = cell.F;
+          currentCell = cell;
+        }
+
+        // Remove currentCell from openList, add to closedList
+        openList.Remove(currentCell);
+        AddCellToClosedList(currentCell);
+      }
+
+      // Get path
+      List<Cell> path = new();
+      currentCell = targetCell;
+
+      while (currentCell.id != startCell.id) {
+        path.Add(currentCell);
+        currentCell = currentCell.parent;
+      }
       path.Add(currentCell);
-      currentCell = currentCell.parent;
-    }
-    path.Add(currentCell);
-    path.Reverse();
+      path.Reverse();
 
-    // Draw path
-    LineRenderer lineRenderer = GetComponent<LineRenderer>();
-    lineRenderer.positionCount = path.Count;
+      // Draw path
+      LineRenderer lineRenderer = GetComponent<LineRenderer>();
+      lineRenderer.positionCount = path.Count;
 
-    for (int i = 0; i < path.Count; i++) {
-      lineRenderer.SetPosition(i, path[i].transform.position + new Vector3(0, 1, 0));
+      for (int i = 0; i < path.Count; i++) {
+        lineRenderer.SetPosition(i, path[i].transform.position + new Vector3(0, 1, 0));
+      }
     }
 
     isCalculating = false;
   }
-  
+
   private List<Cell> GetAdjacentCells(Cell currentCell) {
     return currentCell.GetAdjacentCells(allCells, cellsPerRow);
   }
