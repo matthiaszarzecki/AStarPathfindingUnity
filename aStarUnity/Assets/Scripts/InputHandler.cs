46d05bfba4d380c2ae6fc54e0c43339e0f79fb9c
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2023-02-01 00:25:55 +0100

    Remove collider disabling

--- aStarUnity/Assets/Scripts/InputHandler.cs
+++ aStarUnity/Assets/Scripts/InputHandler.cs
@@ -15,15 +15,11 @@ public class InputHandler : MonoBehaviour {
     if (grabbedCube && !grid.isCalculating) {
       if (Input.GetKey(KeyCode.Mouse0)) {
         Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
-
         if (Physics.Raycast(ray, out RaycastHit hit, 100)) {
-          grabbedCube.GetComponent<Collider>().enabled = false;
           grabbedCube.transform.position = FindClosestCellPosition(hit.point) + new Vector3(0, 1, 0);
         }
       } else if (Input.GetKeyUp(KeyCode.Mouse0)) {
-        grabbedCube.GetComponent<Collider>().enabled = true;
         grabbedCube = null;
-
         grid.CalculatePath();
       }
     }
